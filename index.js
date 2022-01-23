1c8583cf784e1b54bf119ae4418f343f2d6bd913
Santosh Kumawat <santoshkumaawat@gmail.com>
2022-01-23 19:22:09 +0530

    Version 0.1

---
+++ index.js
@@ -0,0 +1,39 @@
+// node server which will handle socket.io
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+
+app.use(require("cors")());
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Method", "GET, POST, PUT, PATCH, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+
+const io = require("socket.io")(8000, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const users = {};
+io.on("connection", (socket) => {
+  socket.on("new-user-joined", (name) => {
+    console.log("newuser ", name);
+
+    users[socket.id] = name;
+    socket.broadcast.emit("user-joined", name);
+  });
+
+ 
+  socket.on("send", (message) => {
+    socket.broadcast.emit("receive", {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+});
---
+++ js/client.js
@@ -0,0 +1,47 @@
+const socket = io("http://localhost:8000");
+
+const form = document.getElementById("send-container");
+
+const messageInput = document.getElementById("messageInp");
+
+const messageContainer = document.querySelector(".message-box");
+
+const UserNameShow = document.getElementById("userName");
+
+var audio = new Audio("ting.mp3");
+
+const append = (message, position) => {
+  const messageElement = document.createElement("div");
+  messageElement.innerText = message;
+  messageElement.classList.add("message");
+  messageElement.classList.add(position);
+
+  messageContainer.append(messageElement);
+  if (position == "left") {
+    audio.play();
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = messageInput.value;
+  append(`You: ${message}`, "right");
+  socket.emit("send", message);
+  messageInput.value = " ";
+});
+
+const name = prompt("Enter Your Name To Join");
+socket.emit("new-user-joined", name);
+UserNameShow.append(name);
+
+socket.on("user-joined", (name) => {
+  append(`${name} joined the chat`, "right");
+});
+
+
+    
+socket.on("receive", (data) => {
+  append(`${data.name}:${data.message}`, "left");
+
+});
