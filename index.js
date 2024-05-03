197bd1c5e45c1798ae3a47d2254747bb170f26ea
Santosh Kumawat <santoshkumaawat@gmail.com>
2024-05-03 17:07:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const path = require("path"); // Add this line
 
 app.use(cors()); 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-// ... (routes like '/test' if needed) 
-
 const httpServer = http.createServer(app);
-
-httpServer.listen(process.env.PORT || 8000, () => {
-  console.log('Server started at ', process.env.PORT || 8000);
-});
 
 const io = require("socket.io")(httpServer, {
   cors: {
@@ -21,36 +16,35 @@ const io = require("socket.io")(httpServer, {
 });
 
 const users = {}; 
-const userRooms = {}; // Track rooms users are in
+const userSockets = {}; // Track sockets of users
 
 io.on("connection", (socket) => {
   socket.on("new-user-joined", (name) => {
     users[socket.id] = name;
+    userSockets[name] = socket; // Associate user name with socket
     socket.broadcast.emit("user-joined", name);
   });
 
-  socket.on("join-private-chat", (otherUserId) => {
-    const roomId = [socket.id, otherUserId].sort().join('-');
+  socket.on("join-private-chat", (otherUserName) => {
+    const roomId = [users[socket.id], otherUserName].sort().join('-');
     socket.join(roomId);
-
-    // Keep track of room association 
-    if (userRooms[socket.id]) {
-      userRooms[socket.id].push(roomId);
-    } else {
-      userRooms[socket.id] = [roomId];
-    }
   });
 
-  socket.on("send-private-message", (message, otherUserId) => {
-    const roomId = [socket.id, otherUserId].sort().join('-');
-    socket.to(roomId).emit("receive-private-message", { 
+  socket.on("send-private-message", (message, otherUserName) => {
+    const roomId = [users[socket.id], otherUserName].sort().join('-');
+    io.to(roomId).emit("receive-private-message", { 
       message: message, 
       name: users[socket.id] 
     });
   });
 
   socket.on('disconnect', () => {
+    const disconnectedUser = users[socket.id];
     delete users[socket.id];
-    // Clean up rooms the user was in (optional, if needed)
+    delete userSockets[disconnectedUser];
   });
-}); 
+});
+
+httpServer.listen(process.env.PORT || 8000, () => {
+  console.log('Server started at ', process.env.PORT || 8000);
+});
