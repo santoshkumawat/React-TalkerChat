0347fc4d66299733d6de4d831a590c72dee58481
Santosh Kumawat <santoshkumaawat@gmail.com>
2024-05-03 17:34:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,24 +6,31 @@ const path = require("path");
 
 const PORT = process.env.PORT || 8000;
 
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle socket connections
 io.on("connection", (socket) => {
   console.log("A user connected");
 
+  // When a new user joins
   socket.on("new-user-joined", (name) => {
+    console.log(name + " joined the chat");
     io.emit("user-joined", name); // Broadcast to all users
   });
 
-  socket.on("send-message", (data) => {
-    io.emit("receive-message", data);
+  // When a user sends a message
+  socket.on("send-message", (message) => {
+    io.emit("receive-message", message);
   });
 
+  // When a user disconnects
   socket.on("disconnect", () => {
     console.log("A user disconnected");
   });
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+// Start the server
 http.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
