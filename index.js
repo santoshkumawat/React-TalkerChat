0daeb8871dc74f159ee61e7a8be846719d16ed57
santosheupheus <santoshkumawat@eupheus.in>
2024-05-16 10:54:27 +0530

    added cors origin

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ app.get('test', (req, res) => {
   return res.send('Hello world');
 })
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Method", "GET, POST, PUT, PATCH, DELETE");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Method", "GET, POST, PUT, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 const httpServer = http.createServer(app);
 httpServer.listen(process.env.PORT || 3000, () => {
